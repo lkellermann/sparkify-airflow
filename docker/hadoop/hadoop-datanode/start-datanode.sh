7b9b57fa136366f962132dfceaeff16fa4d69573
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-11-28 12:21:42 -0300

    add: included hadoop shell files.

---
+++ docker/hadoop/hadoop-datanode/start-datanode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Created by MarcLamberti
+# Maintained by lkellermann
+
+DATA_DIR=$(echo $HDFS_CONF_DFS_DATANODE_DATA_DIR | perl -pe 's#file://##')
+
+if [ ! -d $DATA_DIR ]; then
+    echo "Datanode data directory not found: $DATA_DIR"
+    exit 2
+fi
+
+$HADOOP_HOME/bin/hdfs --config $HADOOP_CONF_DIR datanode
