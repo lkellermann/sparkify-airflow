11148fa4430ceddcf69f5ea76f98f1640cbb5d48
Leandro Kellermann de Oliveira <lk.kellermann@hotmail.com>
2021-12-05 13:12:38 -0300

    fix: changed init-hive-db.sh to run inner upgrade script (iss-1).

--- docker/postgres/init-hive-db.sh
+++ docker/postgres/init-hive-db.sh
@@ -3,6 +3,10 @@
 # Created by MarcLamberti
 # Maintained by lkellermann
 
+# Note: How to setup a PostgreSQL Metastore:
+# https://docs.cloudera.com/cdp-private-cloud-base/7.1.6/hive-metastore/topics/hive-postgres.html
+# /opt/hive the Hive Home directory, in the tutorial above the home is /usr/lib/hhive.
+
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
   DROP DATABASE IF EXISTS metastore;
@@ -11,5 +15,5 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
   CREATE DATABASE metastore;
   GRANT ALL PRIVILEGES ON DATABASE metastore TO hive;
   \c metastore
-  \i /tmp/grant-privs
+  \i /opt/hive/scripts/metastore/upgrade/postgres/hive-schema-3.1.0.postgres.sql
 EOSQL
